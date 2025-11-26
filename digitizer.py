4c80eb700aa1333d61f481801c9653f9b6d301c9
AmirhosseinAsgharnia <asgharnia4@yahoo.com>
2025-11-25 23:59:38 -0500

    added

--- digitizer.py
+++ digitizer.py
@@ -1,1 +1,10 @@
 import cv2 as cv
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="Digitizer program.")
+    parser.add_argument("-i", default="image.jpg", help="Please enter the image name after -i: python /digitizer -i image_name.jpg")
+    args = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
