ce1e7f23b998830db9edfec6720bed8a75c63d06
AmirhosseinAsgharnia <asgharnia4@yahoo.com>
2025-11-26 00:09:19 -0500

    added

--- digitizer.py
+++ digitizer.py
@@ -3,8 +3,18 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="Digitizer program.")
-    parser.add_argument("-i", default="image.jpg", help="Please enter the image name after -i: python /digitizer -i image_name.jpg")
+    parser.add_argument("-i", "--image", default="image.jpg", help="Please enter the image name after -i: python /digitizer -i image_name.jpg")
     args = parser.parse_args()
+
+    image_path = args.image
+    image = cv.imread(image_path)
+
+    if image is None:
+        print("There is no input image")
+        return 0
+
+    cv.imshow("Image", image)
+    cv.waitKey()
 
 if __name__ == "__main__":
     main()
